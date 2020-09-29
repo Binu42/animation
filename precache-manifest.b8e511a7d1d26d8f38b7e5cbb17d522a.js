4d40a310e20d90c066fc7b06ee1ffd0c806a53d4
Binu42 <kbinu42@gmail.com>
2020-09-29 11:04:16 +0530

    Updates

---
+++ precache-manifest.b8e511a7d1d26d8f38b7e5cbb17d522a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f99b7b14893acde3f56fdfe3e26bbc",
+    "url": "/animation/index.html"
+  },
+  {
+    "revision": "96fe2b25612db4067998",
+    "url": "/animation/static/css/main.dc56fa15.chunk.css"
+  },
+  {
+    "revision": "6bc8bbc5e41859ddd1b2",
+    "url": "/animation/static/js/2.bb70b885.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/animation/static/js/2.bb70b885.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fe2b25612db4067998",
+    "url": "/animation/static/js/main.1a25b555.chunk.js"
+  },
+  {
+    "revision": "95435805b786528c2e09",
+    "url": "/animation/static/js/runtime-main.7456037b.js"
+  }
+]);
